fe9e22fd328357ae7d27949774ba5a8b6231cd0e
Carlos Linares <contact@carloslinares.eu>
2023-05-14 10:06:15 +0200

    Initial Commit

---
+++ le01-ex02/le01-ex02.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Carlos Linares on 18.09.2022.
+//
+
+#include "iostream"
+
+int main(){
+    const char name[32] {"Carlos Linares"};
+    const int age = 36;
+
+    std::cout << name << std::endl;
+    std::cout << age;
+
+    return 0;
+}
---
+++ le01-ex03/le01-ex03.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Carlos Linares on 18.09.2022.
+//
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hola Mundo!" << std::endl;
+    return 0;
+}
